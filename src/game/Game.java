817bf5935b44d5353c4cfe95c0c79e9ad9055e0c
nicklas669 <nicklas669@gmail.com>
2014-10-21 21:17:49 +0200

    Baseline med virkende GUI

---
+++ src/game/Game.java
@@ -0,0 +1,10 @@
+package game;
+import boundaryToMatador.GUI;
+public class Game {
+
+	public static void main(String[] args) {
+		GUI.create("fields.txt");
+		GUI.showMessage("Game has begun");
+	}
+
+}
