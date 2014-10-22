5c2018aa6edf69ee4aa177ef46dae6bb98ca5301
Mathias Sondrup <matsondrup@gmail.com>
2014-10-22 14:21:58 +0200

    Added setName and getName

--- src/game/Player.java
+++ src/game/Player.java
@@ -6,4 +6,12 @@ public class Player {
 	public Player(String name) {
 		this.name = name;
 	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
 }
