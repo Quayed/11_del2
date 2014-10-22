4f4382ed118791c91142bd8cc05e439d6d83a113
Mathias Sondrup <matsondrup@gmail.com>
2014-10-22 14:14:40 +0200

    Added deposit method.

--- src/game/Account.java
+++ src/game/Account.java
@@ -6,5 +6,14 @@ public class Account {
 	public Account(int balance) {
 		this.balance = balance;
 	}
-
+	
+	public void deposit(int value){
+		if(value > 0){
+			this.balance = this.balance + value;
+		} else if (this.balance-value > 0){
+			this.balance = this.balance + value;
+		} else if (this.balance-value < 0){
+			this.balance = 0;
+		}
+	}
 }
