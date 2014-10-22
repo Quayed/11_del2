35a1a7d311ca2366a1cedc36a3d4b92e408bcf18
Mathias Sondrup <matsondrup@gmail.com>
2014-10-22 16:04:37 +0200

    added id attribute, as well as getId method.

--- src/game/Player.java
+++ src/game/Player.java
@@ -2,9 +2,15 @@ package game;
 
 public class Player {
 	private String name;
+	final private int id;
 	
-	public Player(String name) {
+	public Player(String name, int id) {
 		this.name = name;
+		this.id = id;
+	}
+
+	public int getId(){
+		return this.id;
 	}
 	
 	public void setName(String name){
