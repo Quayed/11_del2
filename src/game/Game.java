c93d10dc18b7e16a1028ef096308db4b5f3c0d60
nicklas669 <nicklas669@gmail.com>
2014-10-24 08:43:21 +0200

    Added fieldCost array so that we can keep track of what kind of bonus or cost a given field generates. (To avoid reading the cost from fields.txt)

--- src/game/Game.java
+++ src/game/Game.java
@@ -4,6 +4,12 @@ public class Game {
 
 	public static void main(String[] args) {
 		GUI.create("fields.txt");
+		final String[] fieldCost = {"0","250","-200","-100","-20","180","0","-70","-60","-80","-90","650"};
+		
+		for (int i=1; i<fieldCost.length; i++) {
+			System.out.println(i);
+			GUI.setSubText(i, fieldCost[i]);
+		}
 		GUI.showMessage("Game has begun");
 	}
 
