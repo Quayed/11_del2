647ae89559618cf9ca3416c48a05c17e4460061f
RobertEriksen <Robert.b.e@hotmail.com>
2014-10-24 11:50:29 +0200

    Har lavet funktionen 'setDie(a)' der gør at man kan sætte en terning til ethvert heltal

--- src/game/Dice.java
+++ src/game/Dice.java
@@ -5,9 +5,15 @@ import java.util.Random;
 public class Dice {
 	private int die;
 	private Random rnd = new Random();
+	private int a;
 	
 	public int roll() {
 		die = rnd.nextInt(6)+1;
+		return die;
+	}
+	
+	public int setDie(int a){
+		die = a;
 		return die;
 	}
 	
