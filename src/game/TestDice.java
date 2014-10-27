bf6c8560fcf0f4f1226b6e798181e0e01d574336
RobertEriksen <Robert.b.e@hotmail.com>
2014-10-27 12:13:27 +0100

    Har fjernet et print statement der printede elementerne i listen

--- src/game/TestDice.java
+++ src/game/TestDice.java
@@ -17,11 +17,6 @@ public class TestDice {
 			int c = a.roll();
 		liste[b+c-2] = liste[b+c-2]+1;
 		}
-		
-		for(int j=0; j<11; j++){
-			System.out.println(liste[j]);
-		}
-
 		double[] nyliste = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
 		double[] statestik = {1/36.0,2/36.0,3/36.0,4/36.0,5/36.0,6/36.0,5/36.0,4/36.0,3/36.0,2/36.0,1/36.0};
 		for(int i=0; i<11; i++){
