19e21af3f9bda25c5d7caf7640f8e2ca8c9d0bba
Mathias Sondrup <matsondrup@gmail.com>
2014-10-27 16:03:10 +0100

    Changed the language of the code from danish to english.

--- src/game/TestDice.java
+++ src/game/TestDice.java
@@ -6,27 +6,26 @@ import org.junit.Test;
 
 public class TestDice {
 
-	
 	@Test
 	public void test() {
-		int antalkast = 10000000;
-		Dice a = new Dice();
-		int[] liste = {0,0,0,0,0,0,0,0,0,0,0};
-		for(int i=0; i<antalkast; i++){
-			int b = a.roll();
-			int c = a.roll();
-		liste[b+c-2] = liste[b+c-2]+1;
+		int numberOfThrows = 10000000;
+		Dice dice = new Dice();
+		int[] listForValuesOfDice = {0,0,0,0,0,0,0,0,0,0,0};
+		for(int i=0; i<numberOfThrows; i++){
+			int b = dice.roll();
+			int c = dice.roll();
+		listForValuesOfDice[b+c-2] = listForValuesOfDice[b+c-2]+1;
 		}
-		double[] nyliste = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-		double[] statestik = {1/36.0,2/36.0,3/36.0,4/36.0,5/36.0,6/36.0,5/36.0,4/36.0,3/36.0,2/36.0,1/36.0};
+		double[] listOfActualPropability = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+		double[] listOfCalculatedPropability = {1/36.0,2/36.0,3/36.0,4/36.0,5/36.0,6/36.0,5/36.0,4/36.0,3/36.0,2/36.0,1/36.0};
 		for(int i=0; i<11; i++){
-			double e = Math.pow((liste[i] - statestik[i]*antalkast),2);
-			double f = (statestik[i]*antalkast);
-			nyliste[i] = e/f;
+			double e = Math.pow((listForValuesOfDice[i] - listOfCalculatedPropability[i]*numberOfThrows),2);
+			double f = (listOfCalculatedPropability[i]*numberOfThrows);
+			listOfActualPropability[i] = e/f;
 		}
 		double d = 0;
 		for(int i=0; i<11; i++){
-			d = d + nyliste[i];
+			d = d + listOfActualPropability[i];
 		}
 		System.out.println(d);
 		if(d<18.31){
