6bba68ed2fa1c300bdecb77a99e21d842903c811
adamsen9 <adamsen9@gmail.com>
2014-10-28 14:53:55 +0100

    Added whitespaces

--- src/test/TestDice.java
+++ src/test/TestDice.java
@@ -12,23 +12,23 @@ public class TestDice {
 		int numberOfThrows = 10000000;
 		Dice dice = new Dice();
 		int[] listForValuesOfDice = {0,0,0,0,0,0,0,0,0,0,0};
-		double[] listOfActualPropability = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+		double[] listOfActualPropability = {0,0,0,0,0,0,0,0,0,0,0};
 		double[] listOfCalculatedPropability = {1/36.0,2/36.0,3/36.0,4/36.0,5/36.0,6/36.0,5/36.0,4/36.0,3/36.0,2/36.0,1/36.0};
 		double d = 0;
 		
-		for(int i=0; i<numberOfThrows; i++){
+		for(int i = 0; i < numberOfThrows; i++){
 			int b = dice.roll();
 			int c = dice.roll();
 			listForValuesOfDice[b+c-2] = listForValuesOfDice[b+c-2]+1;
 		}
 		
-		for(int i=0; i<11; i++){
+		for(int i = 0; i < 11; i++){
 			double e = Math.pow((listForValuesOfDice[i] - listOfCalculatedPropability[i]*numberOfThrows),2);
 			double f = (listOfCalculatedPropability[i]*numberOfThrows);
 			listOfActualPropability[i] = e/f;
 		}
 		
-		for(int i=0; i<11; i++){
+		for(int i = 0; i < 11; i++){
 			d = d + listOfActualPropability[i];
 		}
 		
